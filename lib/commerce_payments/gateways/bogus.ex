fc7b0da2f2e2042296128d34463980e0440fa084
Josh Nussbaum <joshnuss@gmail.com>
2014-07-10 15:20:16 -0400

    Refactored Bogus gateway

--- lib/commerce_payments/gateways/bogus.ex
+++ lib/commerce_payments/gateways/bogus.ex
@@ -2,38 +2,45 @@ defmodule Commerce.Payments.Gateways.Bogus do
   use Commerce.Payments.Gateways.Base
 
   alias Commerce.Payments.CreditCard
-  alias Commerce.Payments.Address
 
   def purchase(_amount, _card_or_id, _opts) do
-    {:ok, random_string, %{}}
+    success
   end
 
   def authorize(_amount, _card_or_id, _opts) do
-    {:ok, random_string, %{}}
+    success
   end
 
   def capture(id, _opts) do
-    {:ok, id, %{}}
+    success(id)
   end
 
   def void(id, _opts) do
-    {:ok, id, %{}}
+    success(id)
   end
 
-  def refund(amount, id, opts) do
-    {:ok, id, %{}}
+  def refund(_amount, id, _opts) do
+    success(id)
   end
 
   def store(_card=%CreditCard{}, _opts) do
-    {:ok, random_string, %{}}
+    success
   end
 
   def unstore(customer_id, nil, _opts) do
-    {:ok, customer_id, %{}}
+    success(customer_id)
   end
 
   def unstore(_customer_id, card_id, _opts) do
-    {:ok, card_id, %{}}
+    success(card_id)
+  end
+
+  defp success do
+    {:ok, random_string, %{}}
+  end
+
+  defp success(id) do
+    {:ok, id, %{}}
   end
 
   defp random_string(length \\ 10) do
