b08ae269ed17f7bacbc384e9cd0192f78d55cb0a
Josh Nussbaum <joshnuss@gmail.com>
2014-07-15 07:45:01 -0400

    Refactored stripe authorization test to support params

--- test/gateways/stripe_test.exs
+++ test/gateways/stripe_test.exs
@@ -8,12 +8,23 @@ defmodule Commerce.Billing.Gateways.StripeTest do
   alias Commerce.Billing.Response
   alias Commerce.Billing.Gateways.Stripe, as: Gateway
 
-  defmacrop with_request(url, params, status, response, [do: block]) do
+  defmacrop with_request(url, {status, response}, statement, [do: block]) do
     quote do
-      requestFn = fn(:post, unquote(url), unquote(params), [{"Content-Type", "application/x-www-form-urlencoded"}], [hackney: [basic_auth: {'user', 'pass'}]]) ->
+      {:ok, agent} = Agent.start_link(fn -> nil end)
+
+      requestFn = fn(:post, unquote(url), params, [{"Content-Type", "application/x-www-form-urlencoded"}], [hackney: [basic_auth: {'user', 'pass'}]]) ->
+        Agent.update(agent, fn(_) -> params end)
         %{status_code: unquote(status), body: unquote(response)}
       end
-      with_mock HTTPoison, [request: requestFn], do: unquote(block)
+
+      with_mock HTTPoison, [request: requestFn] do
+        unquote(statement)
+        var!(params) = Agent.get(agent, &(URI.decode_query(&1)))
+
+        unquote(block)
+
+        Agent.stop(agent)
+      end
     end
   end
 
@@ -23,7 +34,7 @@ defmodule Commerce.Billing.Gateways.StripeTest do
   end
 
   test "authorize success with credit card", %{config: config} do
-    response = ~S/
+    raw = ~S/
       {
         "id": "1234",
         "card": {
@@ -36,13 +47,21 @@ defmodule Commerce.Billing.Gateways.StripeTest do
     card = %CreditCard{name: "John Smith", number: "123456", cvc: "123", expiration: {2015, 11}}
     address = %Address{}
 
-    with_request "https://api.stripe.com/v1/charges", "capture=false&currency=USD&amount=1095&card%5Bnumber%5D=123456&card%5Bexp_year%5D=2015&card%5Bexp_month%5D=11&card%5Bcvc%5D=123&card%5Bname%5D=John+Smith", 200, response do
-      {:ok, %Response{authorization: authorization,
-                success: success,
-                avs_result: avs_result,
-                cvc_result: cvc_result}} = Gateway.authorize(10.95, card, address: address, config: config)
+    with_request "https://api.stripe.com/v1/charges", {200, raw},
+        response = Gateway.authorize(10.95, card, address: address, config: config) do
+
+      {:ok, %Response{authorization: authorization, success: success,
+                      avs_result: avs_result, cvc_result: cvc_result}} = response
 
       assert success
+      assert params["capture"] == "false"
+      assert params["currency"] == "USD"
+      assert params["amount"] == "1095"
+      assert params["card[name]"] == "John Smith"
+      assert params["card[number]"] == "123456"
+      assert params["card[exp_month]"] == "11"
+      assert params["card[exp_year]"] == "2015"
+      assert params["card[cvc]"] == "123"
       assert authorization == "1234"
       assert avs_result == "P"
       assert cvc_result == "M"
