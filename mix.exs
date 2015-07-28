3de41244b3dd6944ebe24b67b9cef8014e339900
Cory Schmitt <cory@schmitty.me>
2015-07-28 16:32:52 -0700

    Bump version of httpoison to latest

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Commerce.Billing.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:jazz, ">= 0.2.0"},
-     {:httpoison, ">= 0.5.0"},
+     {:httpoison, ">= 0.7.1"},
      {:ex_doc, ">= 0.6.0"},
      {:mock, ">= 0.1.0"}]
   end
