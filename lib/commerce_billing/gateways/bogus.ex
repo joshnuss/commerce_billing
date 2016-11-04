31aa7ac1f2b8907eb5a4867092927775d7ad71bc
Josh Nussbaum <joshnuss@gmail.com>
2016-11-04 17:49:59 -0400

    Refactor bogus gateway

--- lib/commerce_billing/gateways/bogus.ex
+++ lib/commerce_billing/gateways/bogus.ex
@@ -6,47 +6,39 @@ defmodule Commerce.Billing.Gateways.Bogus do
     Response
   }
 
-  def authorize(_amount, _card_or_id, _opts) do
-    success
-  end
+  def authorize(_amount, _card_or_id, _opts),
+    do: success
 
-  def purchase(_amount, _card_or_id, _opts) do
-    success
-  end
+  def purchase(_amount, _card_or_id, _opts),
+    do: success
 
-  def capture(id, _opts) do
-    success(id)
-  end
+  def capture(id, _opts),
+    do: success(id)
 
-  def void(id, _opts) do
-    success(id)
-  end
+  def void(id, _opts),
+    do: success(id)
 
-  def refund(_amount, id, _opts) do
-    success(id)
-  end
+  def refund(_amount, id, _opts),
+    do: success(id)
 
-  def store(_card=%CreditCard{}, _opts) do
-    success
-  end
+  def store(_card=%CreditCard{}, _opts),
+    do: success
 
-  def unstore(customer_id, nil, _opts) do
-    success(customer_id)
-  end
+  def unstore(customer_id, nil, _opts),
+    do: success(customer_id)
 
-  def unstore(_customer_id, card_id, _opts) do
-    success(card_id)
-  end
+  def unstore(_customer_id, card_id, _opts),
+    do: success(card_id)
 
-  defp success do
-    {:ok, Response.success(authorization: random_string)}
-  end
+  defp success,
+    do: {:ok, Response.success(authorization: random_string)}
 
-  defp success(id) do
-    {:ok, Response.success(authorization: id)}
-  end
+  defp success(id),
+    do: {:ok, Response.success(authorization: id)}
 
-  defp random_string(length \\ 10) do
-    1..length |> Enum.map(fn _ -> to_string(:crypto.rand_uniform(0,9)) end) |> Enum.join
-  end
+  defp random_string(length \\ 10),
+    do: 1..length |> Enum.map(&random_char/1) |> Enum.join
+
+  defp random_char(_),
+    do: to_string(:crypto.rand_uniform(0,9))
 end
